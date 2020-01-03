42df09b2145953e6d9c43bc7d258f7af6dcfe4b7
Ofir Lachmi <ofir7909@walla.com>
2020-01-03 16:42:01 +0200

    Sample Code to open a window

---
+++ lib/forge/src/test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace forge{
+
+    int Example();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include <forge.h>
+
 int main(int argc, char** argv)
 {
     std::cout<< "Hello World" <<std::endl;
+    int errorCode = forge::Example();
+
+    return errorCode;
 }
